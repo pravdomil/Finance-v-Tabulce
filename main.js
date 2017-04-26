798777df6a1b37d7023d166388e27179e77e9665
pravdomil <2387356+pravdomil@users.noreply.github.com>
2017-04-26 18:12:06 +0200

    tabs to spaces

--- main.js
+++ main.js
@@ -8,53 +8,53 @@ https://github.com/pravdomil/finance-v-tabulce
 
 // onOpen trigger
 function onOpen() {
-	// simple triggers can't do anything that requires authorization
-	// so make option to install custom onOpen trigger that can do everything
-	SpreadsheetApp.getUi().createMenu('Finance').addItem('Instalovat', 'install').addToUi()
+  // simple triggers can't do anything that requires authorization
+  // so make option to install custom onOpen trigger that can do everything
+  SpreadsheetApp.getUi().createMenu('Finance').addItem('Instalovat', 'install').addToUi()
 }
 
 // custom onOpen trigger
 function customOnOpen() {
-	finInit()
+  finInit()
 }
 
 // daily trigger
 function dailyTrigger() {
-	finDailyTrigger()
+  finDailyTrigger()
 }
 
 // install custom onOpen trigger
 function install() {
-	var ss = SpreadsheetApp.getActiveSpreadsheet()
-	var config = PropertiesService.getDocumentProperties()
-	
-	// setup custom onOpen trigger
-	if(!config.getProperty("openTriggerSet")) {
-		ScriptApp.newTrigger("customOnOpen").forSpreadsheet(ss).onOpen().create()
-		config.setProperty("openTriggerSet", true)
-	}
-	
-	// run custom trigger
-	customOnOpen()
+  var ss = SpreadsheetApp.getActiveSpreadsheet()
+  var config = PropertiesService.getDocumentProperties()
+  
+  // setup custom onOpen trigger
+  if(!config.getProperty("openTriggerSet")) {
+    ScriptApp.newTrigger("customOnOpen").forSpreadsheet(ss).onOpen().create()
+    config.setProperty("openTriggerSet", true)
+  }
+  
+  // run custom trigger
+  customOnOpen()
 }
 
 // google.script.host bridge
 function finBridge(args) {
-	return eval(args.obj)[args.func](args)
+  return eval(args.obj)[args.func](args)
 }
 
 // try to load core if possible
 try {
-	// load core from cdn
-	var core = "https://cdn.rawgit.com/Pravdomil/finance-v-tabulce/master/finance.js"
-	core = UrlFetchApp.fetch(core).getContentText()
-	
-	// run
-	eval(core)
+  // load core from cdn
+  var core = "https://cdn.rawgit.com/Pravdomil/finance-v-tabulce/master/finance.js"
+  core = UrlFetchApp.fetch(core).getContentText()
+  
+  // run
+  eval(core)
 }
 catch(e) {
-	//throw e
-	Logger.log(e)
+  //throw e
+  Logger.log(e)
 }
 
 
