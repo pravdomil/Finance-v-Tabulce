d921245928f4be1955e1ac1e77d284d632f770cd
pravdomil <2387356+pravdomil@users.noreply.github.com>
2021-03-05 23:52:25 +0100

    main.js

--- dist/main.js
+++ dist/main.js
@@ -1,62 +1,39 @@
-/*
-Finance v tabulce
-Authors: Filip Hráček, Pravdomil Toman
+/**
+ * Finance v Tabulce
+ * https://github.com/pravdomil/Finance-v-Tabulce
+ */
 
-https://github.com/pravdomil/finance-v-tabulce
-
-*/
-
-// onOpen trigger
+/**
+ * Runs when a user opens a spreadsheet, document, presentation, or form that the user has permission to edit.
+ * Simple triggers can't do anything that requires authorization.
+ * More information: https://developers.google.com/apps-script/guides/triggers#restrictions
+ */
 function onOpen() {
-  // simple triggers can't do anything that requires authorization
-  // so make option to install custom onOpen trigger that can do everything
-  SpreadsheetApp.getUi()
-    .createMenu("Finance")
-    .addItem("Instalovat", "install")
-    .addToUi()
+  SpreadsheetApp.getUi().createMenu("Finance").addItem("Install", "install").addToUi()
 }
 
-// custom onOpen trigger
-function customOnOpen() {
-  finInit()
-}
-
-// daily trigger
-function dailyTrigger() {
-  finDailyTrigger()
-}
-
-// install custom onOpen trigger
 function install() {
-  var ss = SpreadsheetApp.getActiveSpreadsheet()
-  var config = PropertiesService.getDocumentProperties()
+  const a = SpreadsheetApp.getActiveSpreadsheet()
 
-  // setup custom onOpen trigger
-  if (!config.getProperty("openTriggerSet")) {
-    ScriptApp.newTrigger("customOnOpen")
-      .forSpreadsheet(ss)
-      .onOpen()
-      .create()
-    config.setProperty("openTriggerSet", true)
+  if (ScriptApp.getUserTriggers(a).length === 0) {
+    ScriptApp.newTrigger("openTrigger").forSpreadsheet(a).onOpen().create()
   }
 
-  // run custom trigger
-  customOnOpen()
+  openTrigger()
 }
 
-// google.script.host bridge
-function finBridge(args) {
-  return eval(args.obj)[args.func](args)
+function openTrigger() {
+  run("open")
 }
 
-// try to load core if possible
+function dailyTrigger() {
+  run("daily")
+}
+
 try {
-  // load core from cdn
-  var core = "https://raw.githubusercontent.com/pravdomil/finance-v-tabulce/master/dist/finance.js"
-  core = UrlFetchApp.fetch(core).getContentText()
-
-  // run
-  eval(core)
+  const url = "https://raw.githubusercontent.com/pravdomil/finance-v-tabulce/master/dist/finance.js"
+  const code = UrlFetchApp.fetch(url).getContentText()
+  eval(code)
 } catch (e) {
   // throw e
   Logger.log(e)
