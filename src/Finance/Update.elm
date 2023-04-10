5efc81aee2af629bc536aeea2861c766c52f758e
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-10 21:52:54 +0200

    next

--- src/Finance/Update.elm
+++ src/Finance/Update.elm
@@ -7,5 +7,33 @@ import Task
 
 
 transactions : AppScript.Spreadsheet.Sheet -> List Finance.Config.Config -> Task.Task JavaScript.Error ()
-transactions _ _ =
+transactions sheet configs =
+    Task.sequence
+        [ insertNewTransactions sheet (Finance.Config.fioTokens configs)
+        , updateCells sheet (Finance.Config.rules configs)
+        ]
+        |> Task.map (\_ -> ())
+
+
+
+--
+
+
+insertNewTransactions : AppScript.Spreadsheet.Sheet -> List String -> Task.Task JavaScript.Error ()
+insertNewTransactions sheet tokens =
+    Task.sequence (List.map (insertNewTransactionsHelper sheet) tokens)
+        |> Task.map (\_ -> ())
+
+
+insertNewTransactionsHelper : AppScript.Spreadsheet.Sheet -> String -> Task.Task JavaScript.Error ()
+insertNewTransactionsHelper _ _ =
+    Task.succeed ()
+
+
+
+--
+
+
+updateCells : AppScript.Spreadsheet.Sheet -> List String -> Task.Task JavaScript.Error ()
+updateCells _ _ =
     Task.succeed ()
