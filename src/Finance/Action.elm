b2e87ec4528c5bd9acb5bef00d2c93e3f674895f
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-10 18:32:55 +0200

    next

--- src/Finance/Action.elm
+++ src/Finance/Action.elm
@@ -5,25 +5,30 @@ import Codec
 
 type Action
     = Install
-    | Open
     | Update
+    | OpenTrigger
+    | DailyTrigger
 
 
 codec : Codec.Codec Action
 codec =
     Codec.custom
-        (\fn1 fn2 fn3 x ->
+        (\fn1 fn2 fn3 fn4 x ->
             case x of
                 Install ->
                     fn1
 
-                Open ->
+                Update ->
                     fn2
 
-                Update ->
+                OpenTrigger ->
                     fn3
+
+                DailyTrigger ->
+                    fn4
         )
         |> Codec.variant0 Install
-        |> Codec.variant0 Open
         |> Codec.variant0 Update
+        |> Codec.variant0 OpenTrigger
+        |> Codec.variant0 DailyTrigger
         |> Codec.buildCustom
