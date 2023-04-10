b476ada4ff3e9e1055893a088429e66416ce9741
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-10 17:46:12 +0200

    next

--- src-external/AppScript/Spreadsheet.elm
+++ src-external/AppScript/Spreadsheet.elm
@@ -6,6 +6,36 @@ import Json.Encode
 import Task
 
 
+type Spreadsheet
+    = Spreadsheet Json.Decode.Value
+
+
+
+--
+
+
+type alias Event =
+    { authMode : Json.Decode.Value
+    , source : Spreadsheet
+    , triggerUid : Json.Decode.Value
+    , user : Json.Decode.Value
+    }
+
+
+eventDecoder : Json.Decode.Decoder Event
+eventDecoder =
+    Json.Decode.map4
+        Event
+        (Json.Decode.field "authMode" Json.Decode.value)
+        (Json.Decode.field "source" (Json.Decode.map Spreadsheet Json.Decode.value))
+        (Json.Decode.field "triggerUid" Json.Decode.value)
+        (Json.Decode.field "user" Json.Decode.value)
+
+
+
+--
+
+
 alert : String -> String -> Task.Task JavaScript.Error ()
 alert title description =
     JavaScript.run "SpreadsheetApp.getUi().alert(a[0], a[1])"
