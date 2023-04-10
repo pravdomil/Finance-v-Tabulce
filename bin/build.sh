4a1335539e3fbd7075502062abf0462e7709b292
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-10 15:31:06 +0200

    next

--- bin/build.sh
+++ bin/build.sh
@@ -21,25 +21,6 @@ mkdir -p $dir
 cp -r resources/ dist
 
 # Compile.
-elm make src/Main.elm --output $dir/temp.js --optimize
-elm-ffi $dir/temp.js
-elm-minify $dir/temp.js
-output=$(<$dir/temp.js)
-rm $dir/temp.js
-
-cat << EOF > $dir/index.html
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <title></title>
-    <meta name="viewport" content="width=device-width" />
-  </head>
-  <body>
-    <script>$output</script>
-    <script>
-    (function(){ var a = document.createElement("div"); document.body.appendChild(a); Elm.Main.init({ node: a, flags: { global: this } })})()
-    </script>
-  </body>
-</html>
-EOF
+elm make src/Main.elm --output $dir/v2/elm.js --optimize
+elm-ffi $dir/v2/elm.js
+elm-minify $dir/v2/elm.js
