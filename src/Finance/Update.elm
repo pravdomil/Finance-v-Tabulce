4db7139b14f06449488a83a5177e4c793cc83df0
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-10 22:19:38 +0200

    next

--- src/Finance/Update.elm
+++ src/Finance/Update.elm
@@ -1,9 +1,15 @@
 module Finance.Update exposing (..)
 
 import AppScript.Spreadsheet
+import AppScript.UrlFetch
 import Finance.Config
+import FioCz
+import Iso8601
 import JavaScript
+import Json.Decode
 import Task
+import Time
+import Url
 
 
 transactions : AppScript.Spreadsheet.Sheet -> List Finance.Config.Config -> Task.Task JavaScript.Error ()
@@ -21,13 +27,41 @@ transactions sheet configs =
 
 insertNewTransactions : AppScript.Spreadsheet.Sheet -> List String -> Task.Task JavaScript.Error ()
 insertNewTransactions sheet tokens =
-    Task.sequence (List.map (insertNewTransactionsHelper sheet) tokens)
-        |> Task.map (\_ -> ())
+    Time.now
+        |> Task.andThen
+            (\now ->
+                Task.sequence (List.map (insertNewTransactionsHelper sheet now) tokens)
+                    |> Task.map (\_ -> ())
+            )
 
 
-insertNewTransactionsHelper : AppScript.Spreadsheet.Sheet -> String -> Task.Task JavaScript.Error ()
-insertNewTransactionsHelper _ _ =
-    Task.succeed ()
+insertNewTransactionsHelper : AppScript.Spreadsheet.Sheet -> Time.Posix -> String -> Task.Task JavaScript.Error ()
+insertNewTransactionsHelper _ time token =
+    let
+        url : String
+        url =
+            "https://www.fio.cz/ib_api/rest/periods/"
+                ++ Url.percentEncode token
+                ++ "/"
+                ++ Url.percentEncode (String.left 10 (Iso8601.fromTime (minusDays 30 time)))
+                ++ "/"
+                ++ Url.percentEncode (String.left 10 (Iso8601.fromTime time))
+                ++ "/transactions.json"
+    in
+    AppScript.UrlFetch.fetch url
+        |> Task.andThen
+            (\x ->
+                case Json.Decode.decodeString FioCz.statementDecoder x of
+                    Ok x2 ->
+                        AppScript.Spreadsheet.alert "New Transactions" (Debug.toString x2)
+
+                    Err x2 ->
+                        Task.fail (JavaScript.DecodeError x2)
+            )
+        |> Task.onError
+            (\_ ->
+                AppScript.Spreadsheet.alert "Update Transactions Failed" "Sorry for that."
+            )
 
 
 
@@ -37,3 +71,12 @@ insertNewTransactionsHelper _ _ =
 updateCells : AppScript.Spreadsheet.Sheet -> List String -> Task.Task JavaScript.Error ()
 updateCells _ _ =
     Task.succeed ()
+
+
+
+--
+
+
+minusDays : Int -> Time.Posix -> Time.Posix
+minusDays days a =
+    Time.millisToPosix (Time.posixToMillis a - days * 24 * 60 * 60 * 1000)
