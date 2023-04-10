c4f4f48e0626dff952cb200655d70d5e610cd2bd
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-10 16:18:29 +0200

    next

--- resources/v2/main.js
+++ resources/v2/main.js
@@ -9,7 +9,7 @@ var financeUrl =
   "https://raw.githubusercontent.com/pravdomil/finance-v-tabulce/master/dist/v2/finance.js"
 
 function checkFn() {
-  console.log(Elm ? "Finance is ready." : "Finance is not ready.")
+  console.info(typeof Elm === "object" ? "Finance is ready." : "Finance is not ready.")
 }
 
 try {
