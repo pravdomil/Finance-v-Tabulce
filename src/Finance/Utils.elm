f60a847d1ed46f013d5c327219bfc99f4681352d
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-10 22:38:55 +0200

    next

--- src/Finance/Utils.elm
+++ src/Finance/Utils.elm
@@ -7,20 +7,21 @@ import Time
 valuesToString : List (List AppScript.Spreadsheet.Value) -> String
 valuesToString a =
     let
-        valueToString : AppScript.Spreadsheet.Value -> String
-        valueToString b =
-            case b of
-                AppScript.Spreadsheet.Text c ->
-                    c
-
-                AppScript.Spreadsheet.Number c ->
-                    String.fromFloat c
-
-                AppScript.Spreadsheet.Date c ->
-                    String.fromInt (Time.posixToMillis c)
-
         valuesToString_ : List AppScript.Spreadsheet.Value -> String
         valuesToString_ b =
             String.join "\t" (List.map valueToString b)
     in
     String.join "\n" (List.map valuesToString_ a)
+
+
+valueToString : AppScript.Spreadsheet.Value -> String
+valueToString b =
+    case b of
+        AppScript.Spreadsheet.Text c ->
+            c
+
+        AppScript.Spreadsheet.Number c ->
+            String.fromFloat c
+
+        AppScript.Spreadsheet.Date c ->
+            String.fromInt (Time.posixToMillis c)
