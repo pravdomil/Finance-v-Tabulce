5381d900c1578332c76673f9c975157d7a6eb028
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-11 15:13:37 +0200

    next

--- src/Finance/Category/Utils.elm
+++ src/Finance/Category/Utils.elm
@@ -1,25 +1,14 @@
 module Finance.Category.Utils exposing (..)
 
-import AppScript.Spreadsheet
 import Finance.Category
 import Finance.Column.Utils
+import Finance.UserData
 import Finance.Value.Utils
 import FioCz
-import List.Extra
 
 
-categorize : List Finance.Category.Rule -> FioCz.Transaction -> Finance.Category.Category
-categorize rules a =
-    case List.Extra.find (ruleMatches a) rules of
-        Just b ->
-            Finance.Category.Category (AppScript.Spreadsheet.Text b.category) (AppScript.Spreadsheet.Text b.subcategory)
-
-        Nothing ->
-            Finance.Category.empty
-
-
-ruleMatches : FioCz.Transaction -> Finance.Category.Rule -> Bool
-ruleMatches transaction a =
+ruleMatches : FioCz.Transaction -> Finance.UserData.UserData -> Finance.Category.Rule -> Bool
+ruleMatches transaction data a =
     String.contains
         (String.toLower a.contains)
-        (String.toLower (Finance.Value.Utils.valueToString (Finance.Column.Utils.transactionValue a.column Finance.Category.empty transaction)))
+        (String.toLower (Finance.Value.Utils.valueToString (Finance.Column.Utils.transactionValue a.column data transaction)))
