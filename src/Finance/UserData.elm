e11234578e389cc5d182680a628249435a3fb931
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-11 15:11:36 +0200

    next

---
+++ src/Finance/UserData.elm
@@ -0,0 +1,18 @@
+module Finance.UserData exposing (..)
+
+import AppScript.Spreadsheet
+
+
+type alias UserData =
+    { category : AppScript.Spreadsheet.Value
+    , subcategory : AppScript.Spreadsheet.Value
+    , fulfillmentDate : AppScript.Spreadsheet.Value
+    }
+
+
+empty : UserData
+empty =
+    UserData
+        (AppScript.Spreadsheet.Text "")
+        (AppScript.Spreadsheet.Text "")
+        (AppScript.Spreadsheet.Text "")
