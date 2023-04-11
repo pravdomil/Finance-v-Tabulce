d77676924c9749533bf105792262fffad8f62d79
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-11 13:39:16 +0200

    next

--- src/Finance/Category.elm
+++ src/Finance/Category.elm
@@ -2,12 +2,24 @@ module Finance.Category exposing (..)
 
 import AppScript.Spreadsheet
 import Finance.Column
+import FioCz
+import List.Extra
 
 
 type alias Category =
     { category : AppScript.Spreadsheet.Value
     , subcategory : AppScript.Spreadsheet.Value
     }
+
+
+categorize : List Rule -> FioCz.Transaction -> Category
+categorize rules a =
+    case List.Extra.find (ruleMatches a) rules of
+        Just b ->
+            Category (AppScript.Spreadsheet.Text b.category) (AppScript.Spreadsheet.Text b.subcategory)
+
+        Nothing ->
+            Category (AppScript.Spreadsheet.Text "") (AppScript.Spreadsheet.Text "")
 
 
 
