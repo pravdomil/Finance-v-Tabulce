dedf82a23bbcfd7d92a5e954638ecf45f0b634fc
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-11 20:11:10 +0200

    next

---
+++ dist/v2/main.js
@@ -0,0 +1,34 @@
+/*
+ * Finance v Tabulce
+ *
+ * https://github.com/pravdomil/finance-v-tabulce
+ *
+ */
+
+var financeUrl =
+  "https://raw.githubusercontent.com/pravdomil/finance-v-tabulce/master/dist/v2/finance.js"
+
+function onOpen() {
+  // simple triggers can't do anything that requires authorization
+  SpreadsheetApp.getUi().createMenu("Finance v Tabulce").addItem("Install", "onInstallAction").addToUi()
+}
+
+function onInstallAction(e) {
+  eval(UrlFetchApp.fetch(financeUrl).getContentText())
+  Elm.Main.init({ flags: { action: ["v1", [0, e]] } })
+}
+
+function onUpdateAction(e) {
+  eval(UrlFetchApp.fetch(financeUrl).getContentText())
+  Elm.Main.init({ flags: { action: ["v1", [1, e]] } })
+}
+
+function onOpenTrigger(e) {
+  eval(UrlFetchApp.fetch(financeUrl).getContentText())
+  Elm.Main.init({ flags: { action: ["v1", [2, e]] } })
+}
+
+function onDailyTrigger(e) {
+  eval(UrlFetchApp.fetch(financeUrl).getContentText())
+  Elm.Main.init({ flags: { action: ["v1", [3, e]] } })
+}
