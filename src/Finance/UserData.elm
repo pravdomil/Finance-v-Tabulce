98069ac25b892508a1ccb7e764389e683fcb7a38
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-11 15:32:16 +0200

    next

--- src/Finance/UserData.elm
+++ src/Finance/UserData.elm
@@ -7,12 +7,14 @@ type alias UserData =
     { category : AppScript.Spreadsheet.Value
     , subcategory : AppScript.Spreadsheet.Value
     , fulfillmentDate : AppScript.Spreadsheet.Value
+    , note : AppScript.Spreadsheet.Value
     }
 
 
 empty : UserData
 empty =
     UserData
+        (AppScript.Spreadsheet.Text "")
         (AppScript.Spreadsheet.Text "")
         (AppScript.Spreadsheet.Text "")
         (AppScript.Spreadsheet.Text "")
