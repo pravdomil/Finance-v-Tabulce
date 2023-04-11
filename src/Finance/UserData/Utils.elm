0f69348fe23bf0979957fda67abc9df914cd84aa
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-11 16:18:07 +0200

    next

--- src/Finance/UserData/Utils.elm
+++ src/Finance/UserData/Utils.elm
@@ -3,12 +3,12 @@ module Finance.UserData.Utils exposing (..)
 import AppScript.Spreadsheet
 import Finance.Category
 import Finance.Category.Utils
+import Finance.Transaction
 import Finance.UserData
-import FioCz
 import List.Extra
 
 
-categorize : List Finance.Category.Rule -> FioCz.Transaction -> Finance.UserData.UserData -> Finance.UserData.UserData
+categorize : List Finance.Category.Rule -> Finance.Transaction.Transaction -> Finance.UserData.UserData -> Finance.UserData.UserData
 categorize rules transaction a =
     case List.Extra.find (Finance.Category.Utils.ruleMatches transaction a) rules of
         Just b ->
