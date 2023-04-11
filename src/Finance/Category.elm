41328ab4162d57fe43a994f6c7ff16ab5dea246f
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-11 15:11:58 +0200

    next

--- src/Finance/Category.elm
+++ src/Finance/Category.elm
@@ -1,22 +1,6 @@
 module Finance.Category exposing (..)
 
-import AppScript.Spreadsheet
 import Finance.Column
-
-
-type alias Category =
-    { category : AppScript.Spreadsheet.Value
-    , subcategory : AppScript.Spreadsheet.Value
-    }
-
-
-empty : Category
-empty =
-    Category (AppScript.Spreadsheet.Text "") (AppScript.Spreadsheet.Text "")
-
-
-
---
 
 
 type alias Rule =
