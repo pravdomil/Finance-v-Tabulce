d68c1f998f8b0b3df5c51311f7e70599f1957594
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-12 15:39:38 +0200

    next

--- src/Finance/UserData/Utils.elm
+++ src/Finance/UserData/Utils.elm
@@ -1,6 +1,7 @@
 module Finance.UserData.Utils exposing (..)
 
 import AppScript.Spreadsheet
+import Finance.AccountName
 import Finance.Category
 import Finance.Category.Utils
 import Finance.Transaction
@@ -8,9 +9,9 @@ import Finance.UserData
 import List.Extra
 
 
-categorize : List Finance.Category.Rule -> Finance.Transaction.Transaction -> Finance.UserData.UserData -> Finance.UserData.UserData
-categorize rules transaction a =
-    case List.Extra.find (Finance.Category.Utils.ruleMatches transaction a) rules of
+categorize : Finance.AccountName.Database -> List Finance.Category.Rule -> Finance.Transaction.Transaction -> Finance.UserData.UserData -> Finance.UserData.UserData
+categorize accountNames rules transaction a =
+    case List.Extra.find (Finance.Category.Utils.ruleMatches accountNames transaction a) rules of
         Just b ->
             { a
                 | category = AppScript.Spreadsheet.Text b.category
