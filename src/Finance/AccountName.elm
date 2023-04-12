9499ca05332cb04e06ebbcf2be48aa08e9219d56
pravdomil <2387356+pravdomil@users.noreply.github.com>
2023-04-12 14:20:15 +0200

    next

--- src/Finance/AccountName.elm
+++ src/Finance/AccountName.elm
@@ -1,7 +1,22 @@
 module Finance.AccountName exposing (..)
 
+import Dict
+
 
 type alias AccountName =
     { account : String
     , name : String
     }
+
+
+
+--
+
+
+type alias Database =
+    Dict.Dict String String
+
+
+accountNamesToDatabase : List AccountName -> Dict.Dict String String
+accountNamesToDatabase a =
+    Dict.fromList (List.map (\x -> ( x.account, x.name )) a)
